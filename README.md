e8b9de7560f499efdbe294844010ba5fe9e48655
mrdrivingduck <562655624@qq.com>
2019-05-06 23:42:43 +0800

    Extracting frame string

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # beacon-frame-analysis
-🦜 Scripts for analyzing 802.11 beacon frames.
+🐌 Scripts for analyzing 802.11 beacon frames.
+
+Created by : Mr Dk.
+
+2019 / 05 / 05
+
+@Nanjing, Jiangsu, China
+
+---
+
+## About
+
+Python scripts for analyzing IEEE 802.11 beacon frames.
+
+---
+
+## Dependency Tools
+
+* _[pyshark](https://github.com/KimiNewt/pyshark)_ - for analyzing 802.11 beacon frames
+* _[pandas](<https://github.com/pandas-dev/pandas>)_ - for processing data
+* _[sklearn](<https://github.com/scikit-learn/scikit-learn>)_ - for machine learning
+
+---
+
--- src/process/extract.py
+++ src/process/extract.py
@@ -1,6 +1,6 @@
 """
     @author mrdrivingduck
-    @version 2019-05-05
+    @version 2019-05-06
     @description
         To extract features from beacon frames using pyshark
 """
@@ -13,9 +13,20 @@ ap_name = 'mrdrivingduck'
 ap_name = 'surface-soft'
 frames = pyshark.FileCapture('data/beacon-' + ap_name + '.pcap')
 
-rssi_arr = [pkt.radiotap.dbm_antsignal for pkt in frames]
-df = pd.DataFrame({"rssi": rssi_arr})
-print(df)
-df.to_csv('out/' + ap_name + '.csv')
+"""
+    @function Extracting RSSI
+"""
+# rssi_arr = [pkt.radiotap.dbm_antsignal for pkt in frames]
+# df = pd.DataFrame({"rssi": rssi_arr})
+# print(df)
+# df.to_csv('out/' + ap_name + '.csv')
+
+"""
+    @function Extracting fixed fields in string format
+"""
+for pkt in frames:
+    print(pkt[3].__str__())
+    print(pkt[3].pretty_print())
+
 
 frames.close()
